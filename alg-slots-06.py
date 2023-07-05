546d034550ce77345f241a31e360702b72c198a2
Ivam Galvão Filho <programador.ivam@gmail.com>
2023-07-05 18:31:38 -0300

    add new code and settings in algorithm 06

--- alg-slots-06.py
+++ alg-slots-06.py
@@ -5,7 +5,12 @@
 import os
 import xml.etree.ElementTree as ET
 import webbrowser
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+import string
 
+# Directory path
 path = "C:\\Dataset-TRT"
 files = os.listdir(path)
 
@@ -18,6 +23,9 @@ output_html += "<h3>Arquivos encontrados no diretório:</h3>"
 
 slot_number = 1
 
+# Functions
+
+# Function to replace words
 def replace_words(text):
     word_replacements = {
         "LimitaÃ§Ã£o da condenaÃ§Ã£o aos valores dos pedidos": "Limitacao da condenacao aos valores dos pedidos",
@@ -43,6 +51,7 @@ def replace_words(text):
         text = text.replace(old_word, new_word)
     return text
 
+# Function to replace expressions
 def replace_expression(text):
     expressions = {
         "LimitaÃ§Ã£o da condenaÃ§Ã£o aos valores dos pedidos": "Limitacao da condenacao aos valores dos pedidos",
@@ -57,6 +66,13 @@ def replace_expression(text):
         text = text.replace(expression, replacement)
     return text
 
+# Tokenize the sentences into words
+def tokenize_sentence(sentence):
+    tokens = word_tokenize(sentence)
+    tokens = [token.lower() for token in tokens if token.lower() not in stopwords.words('portuguese') and token not in string.punctuation]
+    return tokens
+
+# Loop through files in directory
 for file in files:
     if file.endswith(".xml"):
         output_html += f"<p>{file}</p>"
@@ -67,58 +83,58 @@ for file in files:
         output_html += f"<h4>Conteúdo do arquivo {file}:</h4>"
         sentences = []
 
+        # Loop throughsentences
         for sentence in root.iter('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence'):
             tokens = []
             for token in sentence.iter('de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token'):
                 tokens.append(token.text.strip())
 
+            # Checks if the sentence contains the specific tags
             if sentence.find(".//webanno.custom.Judgmentsentity") is not None:
-                annotated_word = sentence.find(".//webanno.custom.Judgmentsentity/de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token").text.strip()
-                annotated_word = replace_expression(annotated_word)
+                annotated_word = sentence.find(
+                    ".//webanno.custom.Judgmentsentity/de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token").text.strip()
+                annotated_word = replace_expression(annotated_word)  # Substitui o conteúdo de annotated_word
+
                 sentence_text = ' '.join(tokens)
 
+                # Checks if the sentence has already been added
                 if sentence_text not in sentences:
                     sentences.append(sentence_text)
 
+                    # Apply word replacements
                     sentence_text = replace_words(sentence_text)
 
-                    if annotated_word in sentence_text:
-                        start_index = sentence_text.find(annotated_word)
-                        end_index = start_index + len(annotated_word)
+                    # Tokenize the sentences
+                    sentences_list = sent_tokenize(sentence_text)
 
-                        # Procura até 5 palavras antes da expressão anotada
-                        for i in range(1, 6):
-                            if start_index - i >= 0 and sentence_text[start_index - i].isalnum():
-                                start_index -= 1
+                    # Prints the sentences and the annotated word
+                    for sent_idx, sent in enumerate(sentences_list[:5]):  # Select up to 5 sentences
+                        tokenized_sent = tokenize_sentence(sent)
+                        annotated_index = tokenized_sent.index(
+                            annotated_word.lower()) if annotated_word.lower() in tokenized_sent else -1
+                        context_start = max(0, annotated_index - 5)
+                        context_end = min(annotated_index + 6, len(tokenized_sent))
+                        context_words = tokenized_sent[context_start:context_end]
 
-                        # Procura até 5 palavras depois da expressão anotada
-                        for i in range(1, 6):
-                            if end_index + i < len(sentence_text) and sentence_text[end_index + i].isalnum():
-                                end_index += 1
-
-                        slot_part_before = sentence_text[start_index:end_index].strip()
-
-                        # Verifica se há espaço suficiente para obter 5 palavras após a expressão anotada
-                        if end_index + 5 < len(sentence_text):
-                            slot_part_after = sentence_text[end_index:end_index + 5].strip()
-                        else:
-                            slot_part_after = sentence_text[end_index:].strip()
-
-                        slot_part = slot_part_before + " " + slot_part_after
-
-                        output_html += f"<p>Slot {slot_number}: {slot_part}</p>"
+                        context_text = ' '.join(context_words)
+                        output_html += f"<p>Sentença {slot_number}: {context_text}</p>"
                         output_html += f"<p>Annotated Word: {annotated_word}</p>"
+                        output_html += f"<p>Tokens: {context_words}</p>"  # Print the token vector
                         slot_number += 1
 
+# Output files path
 output_file_txt = os.path.join(output_dir, "output.txt")
 output_file_html = os.path.join(output_dir, "output.html")
 
+# Save the result to the output TXT file
 with open(output_file_txt, "w", encoding="utf-8") as f:
     f.write(output_html)
 
+# Save the result to the HTML output file
 with open(output_file_html, "w", encoding="utf-8") as f:
     f.write(output_html)
 
+# Opens the HTML file in the browser
 webbrowser.open(output_file_html)
 
 print("Results saved in folder C://Outputs")
